12d9c6a3e21f1a8e0426d35c60d0039380e2dd9c
Lilac <jonasjosh7@gmail.com>
2023-05-25 15:21:08 -0600

    functionality finished

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,8 @@
+var searchHistory = JSON.parse(localStorage.getItem("cityHistory")) || []
+
+
+
+
 // Function to fetch weather forecast for a given city
 async function fetchWeatherForecast(city) {
     const apiKey = 'bef51ab856b8754ff342fe17908eb5cc'; 
@@ -11,21 +16,27 @@ async function fetchWeatherForecast(city) {
       }
   
       const data = await response.json();
-      return data;
+      console.log(data);
+      var days=[]
+      for (var i=0; i<data.list.length; i+=7){
+        days.push(data.list[i])
+      }
+      console.log(days);
+      displayWeatherForecast(data, days);
     } catch (error) {
       console.error(error);
     }
   }
   
   // Function to display weather forecast
-  function displayWeatherForecast(data) {
+  function displayWeatherForecast(data, days) {
     const forecastContainer = document.getElementById('weather-forecast');
     // Clear previous forecast data
     forecastContainer.innerHTML = '';
   
     // Extract necessary information from the data
     const city = data.city.name;
-    const forecastList = data.list;
+    const forecastList = days;
   
     // Create HTML elements to display the forecast information
     const heading = document.createElement('h2');
@@ -44,13 +55,16 @@ async function fetchWeatherForecast(city) {
       const date = new Date(forecast.dt_txt);
       const dateString = date.toLocaleDateString(undefined, { weekday: 'long', month: 'long', day: 'numeric' });
   
-      const temperature = Math.round(forecast.main.temp - 273.15); // Convert temperature from Kelvin to Celsius
+      const temperature = Math.round((forecast.main.temp - 273.15) * 9/5 + 32); // Convert temperature from Kelvin to Celsius
       const wind = forecast.wind.speed;
       const humidity = forecast.main.humidity;
-  
+    const icon = forecast.weather[0].icon
+
+
       forecastItem.innerHTML = `
         <p>Date: ${dateString}</p>
-        <p>Temperature: ${temperature} °C</p>
+        <img class="icon" src ="https://openweathermap.org/img/wn/${icon}@2x.png"/>
+        <p>Temperature: ${temperature} °F</p>
         <p>Wind: ${wind} m/s</p>
         <p>Humidity: ${humidity}%</p>
       `;
@@ -70,7 +84,24 @@ async function fetchWeatherForecast(city) {
     const searchBar = document.getElementById('search-bar');
     const city = searchBar.value.trim();
   
+
+
     if (city) {
       fetchWeatherForecast(city)
-        .then
-  
+        searchHistory.push(city)
+        localStorage.setItem("cityHistory", JSON.stringify(searchHistory))
+    }
+}
+if (searchHistory.length>0){
+for (var i=0; i<searchHistory.length; i++){
+    const historyButton = document.createElement("button")
+    historyButton.textContent = searchHistory[i]
+    document.querySelector("#historyContainer").appendChild(historyButton)
+}
+}
+document.querySelector("#city-search").addEventListener("click", handleSearch);
+document.querySelector("#historyContainer").addEventListener("click", function(event){
+console.log(event.target.textContent)
+const city = event.target.textContent
+fetchWeatherForecast(city)
+})
